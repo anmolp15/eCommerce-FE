07ff7734d20610a23757274fccc1b2269d484748
Anmol Pandey <anmolpandey@Anmols-MacBook-Air.local>
2022-07-03 06:22:26 +0530

    add components and make routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 import "../App.css";
+import {Routes, Route} from 'react-router-dom'
 import NavBar from "./NavBar";
+import Home from './Home'
+import CreateProduct from './CreateProduct'
+import UpdateProduct from './UpdateProduct'
+import Cart from './Cart'
+import ProductPage from './ProductPage'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <div>hello world..</div>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/products/:prodId" element={<ProductPage/>}/>
+        <Route exact path="/create-product" element={<CreateProduct/>}/>
+        <Route exact path="/update-product/:prodId" element={<UpdateProduct/>}/>
+        <Route exact path="/cart" element={<Cart/>}/>
+      </Routes>
+      
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import styles from "../css/navbar.module.css";
 import Axios from "axios";
-
-const BASE_URL = "https://fakestoreapi.com/products";
+import { BASE_URL } from "../utils";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
   const [categories, setCategories] = useState([]);
+
   useEffect(() => {
     let url = `${BASE_URL}/categories`;
     Axios.get(url).then((categ) => setCategories(categ.data));
@@ -13,10 +14,8 @@ export default function NavBar() {
 
   function listCategories() {
     return (
-      <select name="category" id="category">
-        <option value="none" selected disabled>
-          All categories
-        </option>
+      <select name="category" id="category" defaultValue="All categories">
+        <option value="all-categories">All Categories</option>
         {categories.map((item, index) => {
           return (
             <option value="item" key={index}>
@@ -27,15 +26,20 @@ export default function NavBar() {
       </select>
     );
   }
+
   return (
     <div>
       <div className={styles.container}>
-        <div className={styles.logo}>E Commerce</div>
+        <div className={styles.logo}>
+          <Link to="/">E Commerce</Link>
+        </div>
 
         <div className={styles.utilities}>
           <div className={styles.categories}>{listCategories()}</div>
           <div className={styles.cart}>
-            <i className="fa fa-shopping-cart" aria-hidden="true"></i>
+            <Link to="/cart">
+              <i className="fa fa-shopping-cart" aria-hidden="true"></i>
+            </Link>
           </div>
           <div className={styles.user}>
             <i className="fa fa-user-circle" aria-hidden="true"></i>
